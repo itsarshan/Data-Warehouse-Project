bf92a419d015daa60ac1bc88f5f08805c08e91dd
Arshan Alam <arshan131004@gmail.com>
2026-05-23 13:24:53 +0530

    Create dim_products.sql

---
+++ scripts/bronze/dim_products.sql
@@ -0,0 +1,40 @@
+/*
+===============================================================================
+Create Product Dimension View
+===============================================================================
+
+Script Purpose:
+    This script creates the Product Dimension view in the Gold Layer.
+    The view transforms and enriches product data by joining CRM
+    and ERP source systems.
+
+    This dimension table is used in the Star Schema model for
+    analytical reporting and business intelligence.
+
+===============================================================================
+*/
+
+CREATE VIEW gold.dim_products AS
+
+SELECT
+    ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
+
+    pn.prd_id AS product_id,
+    pn.prd_key AS product_number,
+    pn.prd_nm AS product_name,
+    pn.cat_id AS category_id,
+
+    pc.cat AS category,
+    pc.subcat AS subcategory,
+    pc.maintenance,
+
+    pn.prd_cost AS cost,
+    pn.prd_line AS product_line,
+    pn.prd_start_dt AS start_date
+
+FROM silver.crm_prd_info pn
+
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+    ON pn.cat_id = pc.id
+
+WHERE pn.prd_end_dt IS NULL;
